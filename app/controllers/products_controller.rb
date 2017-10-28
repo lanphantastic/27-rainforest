23fe8168f6bb136f0c092632a7dac6aacbdb6498
Lan Phan <lanc_phan@hotmail.com>
2017-10-27 22:21:54 -0400

    make the new form for view and set up @product in the new and create controller method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,6 +13,11 @@ class ProductsController < ApplicationController
   end
 
   def create
+    @product = Product.new
+
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
 
   end
 
