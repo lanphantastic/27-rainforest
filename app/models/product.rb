51149d59542806d1a6be167d915e220a17c8fee1
Lan Phan <lanc_phan@hotmail.com>
2017-10-27 21:08:15 -0400

    created the seven rails routes in the products' controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,30 @@
 class Product < ApplicationRecord
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
