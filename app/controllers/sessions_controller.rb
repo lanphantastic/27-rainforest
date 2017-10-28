fcb2ca7232783411acfc03ef4a43ecb09bcba7dc
Lan Phan <lanc_phan@hotmail.com>
2017-10-28 18:14:15 -0400

    made the 'create' controller method for session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    u = User.find_by(email: params[:session][:email])
+
+    if u && u.authenticate(params[:session][:password])
+      redirect_to root_url
+    else
+      flash[:alert] = "You need to sign up!"
+      render :new
+    end
   end
 
   def destroy
