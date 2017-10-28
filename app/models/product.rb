6dee070ca8ce0f80876ba79d11c12e91a00504cd
Lan Phan <lanc_phan@hotmail.com>
2017-10-27 23:07:43 -0400

    generate a review model with the product_id, and add assocations to Product and Review models to reflect this relationship

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :reviews
+
   validates :name, :description, :price_in_cents, presence: {message: "Please fill in the %{attribute}"}
   validates :price_in_cents, numericality: true
 
@@ -6,4 +8,5 @@ class Product < ApplicationRecord
     price_in_dollars = price_in_cents.to_f / 100
     sprintf("$%.2f", price_in_dollars)
   end
+  
 end
