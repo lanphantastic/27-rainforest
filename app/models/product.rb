c1e010eb3fd2af59a68a562daeb0e4630a88bc98
Lan Phan <lanc_phan@hotmail.com>
2017-10-27 22:08:53 -0400

    add validations to the product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
-
+  validates :name, :description, :price_in_cents, presence: {message: "Please fill in the %{attribute}"}
+  validates :price_in_cents, numericality: { message: "Please enter a integer"}
 end
