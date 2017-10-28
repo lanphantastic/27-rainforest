51c54542e5932a4fe0fba5c8f57b60fcbed22243
Lan Phan <lanc_phan@hotmail.com>
2017-10-28 17:13:43 -0400

    add 'has_secure_password' and a validation to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true
+  # with has_secure_password, the following validations are added automatically: password must be present on creation, password length should be <= 72 characters, and confirmation of password (using a password_confirmation attribute)
+
+  # NOTE: has_secure_password and validates are methods
 end
