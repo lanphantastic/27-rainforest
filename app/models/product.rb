5fd4c8752cbb10dc9b6c78d16fabae2837fdc8cc
Lan Phan <lanc_phan@hotmail.com>
2017-10-27 22:56:16 -0400

    displaying prices in dollars in the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
   validates :name, :description, :price_in_cents, presence: {message: "Please fill in the %{attribute}"}
   validates :price_in_cents, numericality: true
+
+  def price_in_dollars
+    price_in_dollars = price_in_cents.to_f / 100
+    sprintf("$%.2f", price_in_dollars)
+  end
 end
