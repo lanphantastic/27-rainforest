61722bef8f6f839730bd3e4ddc1194c1229a2867
Lan Phan <lanc_phan@hotmail.com>
2017-10-27 22:47:46 -0400

    add edit, back, delete link, add update and destroy controller methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -29,14 +29,35 @@ class ProductsController < ApplicationController
   end
 
   def edit
-
+    @product = Product.find(params[:id])
   end
 
   def update
 
+    @product = Product.find(params[:id])
+
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+
+    if @product.save
+      flash[:notice] = "Product has been successfully updated."
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
   end
 
   def destroy
 
+    if @product.destroy
+      flash[:notice] = "Product has been successfully deleted."
+      redirect_to products_path
+      flash[:notice] = "You have deleted the product #{product.name}."
+    else
+      render :show
+    end
+
   end
+
 end
