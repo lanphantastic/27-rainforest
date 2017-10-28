100d7620b242eb1dea23e503e44b94fac55aa22d
Lan Phan <lanc_phan@hotmail.com>
2017-10-27 21:24:49 -0400

    added four films to seed and confirmed it in rails console with 'Product.all'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Remove all previous loaded products
+Product.destroy_all
+
+
+Product.create(
+  name: 'Star Wars',
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut ligula dui. Maecenas in sapien cursus, condimentum nulla vitae, pellentesque metus. Sed accumsan tellus et molestie volutpat. Maecenas feugiat, mi.",
+  price_in_cents: 299
+)
+
+Product.create(
+  name: 'Lord of the Rings',
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut ligula dui. Maecenas in sapien cursus, condimentum nulla vitae, pellentesque metus. Sed accumsan tellus et molestie volutpat. Maecenas feugiat, mi.",
+  price_in_cents: 299
+)
+
+Product.create(
+  name: 'Fast and Furious',
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut ligula dui. Maecenas in sapien cursus, condimentum nulla vitae, pellentesque metus. Sed accumsan tellus et molestie volutpat. Maecenas feugiat, mi.",
+  price_in_cents: 299
+)
+
+Product.create(
+  name: 'Interstellar',
+  description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ut ligula dui. Maecenas in sapien cursus, condimentum nulla vitae, pellentesque metus. Sed accumsan tellus et molestie volutpat. Maecenas feugiat, mi.",
+  price_in_cents: 299
+)
