829678abf74690eeca29146a021b7c0deaf787cc
Lan Phan <lanc_phan@hotmail.com>
2017-10-28 18:25:16 -0400

    add a session hash assigned it to the user's id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,23 @@ class SessionsController < ApplicationController
     u = User.find_by(email: params[:session][:email])
 
     if u && u.authenticate(params[:session][:password])
+
+      # Put the wristband on the User
+      session[:user_id] = u.id
       redirect_to root_url
     else
       flash[:alert] = "You need to sign up!"
       render :new
     end
-    
+
   end
 
   def destroy
   end
 end
+
+# There are three magic hashes available to you in a Rails Controller
+
+# params
+# session
+# flash
