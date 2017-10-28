84020c8ef9d4cbf2e0ad9e8bd0937c4a7166230e
Lan Phan <lanc_phan@hotmail.com>
2017-10-27 21:05:41 -0400

    add resources :products for rails routes

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20171028005703) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.text "description"
+    t.text    "description"
     t.integer "price_in_cents"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
